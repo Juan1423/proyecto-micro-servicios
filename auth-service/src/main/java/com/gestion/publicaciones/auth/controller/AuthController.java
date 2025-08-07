41c305ab68921e47d07dd5a8d0820649491ff987
Juan Ocaña <juandy.099@gmail.com>
2025-08-06 22:51:20 -0500

    Agregado de la ruta para la verificacion en application.yml y auth controller

--- auth-service/src/main/java/com/gestion/publicaciones/auth/controller/AuthController.java
+++ auth-service/src/main/java/com/gestion/publicaciones/auth/controller/AuthController.java
@@ -2,10 +2,13 @@ package com.gestion.publicaciones.auth.controller;
 
 
 
+import com.gestion.publicaciones.auth.config.JwtUtil;
 import com.gestion.publicaciones.auth.service.AuthService;
 import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 @RestController
@@ -41,4 +44,11 @@ public class AuthController {
         public String email;
         public String password;
     }
+
+    @GetMapping("/.well-known/jwks.json")
+    public Map<String, Object> getJwk() {
+        JwtUtil jwtUtil = new JwtUtil();
+        return Map.of("keys", List.of(jwtUtil.getJwk().toJSONObject()));
+    }
+
 }
