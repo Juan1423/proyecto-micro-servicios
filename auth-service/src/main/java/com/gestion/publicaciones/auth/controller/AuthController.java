9de13dc21a83fb4bf88c0e26e59f48798921ecff
Juan Ocaña <juandy.099@gmail.com>
2025-08-06 21:35:11 -0500

    Creacion del controlador y el repositorio

---
+++ auth-service/src/main/java/com/gestion/publicaciones/auth/controller/AuthController.java
@@ -0,0 +1,44 @@
+package com.gestion.publicaciones.auth.controller;
+
+
+
+import com.gestion.publicaciones.auth.service.AuthService;
+import lombok.Data;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return authService.registrar(request.nombre, request.email, request.password, request.roles);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return authService.login(request.email, request.password);
+    }
+
+    @Data
+    static class RegisterRequest {
+        public String nombre;
+        public String email;
+        public String password;
+        public Set<String> roles;
+    }
+
+    @Data
+    static class LoginRequest {
+        public String email;
+        public String password;
+    }
+}
