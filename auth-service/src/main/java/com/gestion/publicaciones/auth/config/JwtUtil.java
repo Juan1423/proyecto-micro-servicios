d43ccebd47f7de1deff8934fb6a9084165f8fd6f
Juan Ocaña <juandy.099@gmail.com>
2025-08-06 21:26:14 -0500

    Archivos de configuracion y modificaiones al main del servicio

---
+++ auth-service/src/main/java/com/gestion/publicaciones/auth/config/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.gestion.publicaciones.auth.config;
+
+
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.Set;
+import java.util.UUID;
+
+@Component
+public class JwtUtil {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+    private final long expiration = 1000 * 60 * 60; // 1 hora
+
+    public String generateToken(UUID userId, Set<String> roles) {
+        return Jwts.builder()
+                .setSubject(userId.toString())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + expiration))
+                .signWith(key)
+                .compact();
+    }
+
+    public Key getPublicKey() {
+        return key;
+    }
+}
